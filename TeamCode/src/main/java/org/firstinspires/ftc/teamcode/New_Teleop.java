fc32cc537db48ae0c114a2d5853671481071aef6
Lavanthi <80131633+Lavanthi@users.noreply.github.com>
2025-09-17 19:21:23 -0400

    Update New_Teleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/New_Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/New_Teleop.java
@@ -1,4 +1,13 @@
+// a package defines where our code is stored
 package org.firstinspires.ftc.TeamCode;
 
-public class New_Teleop {
+// imports bring in LinearOpMode, motors, servos, etc.
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.robotcore.external.JavaUtil;
+
+public class New_Teleop extends LinearOpMode {
+  
 }
+
